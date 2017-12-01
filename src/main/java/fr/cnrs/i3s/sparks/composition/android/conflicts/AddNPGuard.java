720cf8d50883c6c687c866db3152cc1e8c6e17d9
Benjamin Benni <benni@i3s.unice.fr>
2017-12-01 01:07:43 +0100

    Update guard

--- src/main/java/fr/cnrs/i3s/sparks/composition/android/conflicts/AddNPGuard.java
+++ src/main/java/fr/cnrs/i3s/sparks/composition/android/conflicts/AddNPGuard.java
@@ -4,42 +4,54 @@ import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtIf;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtExecutable;
+import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtParameter;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import static fr.cnrs.i3s.sparks.composition.android.conflicts.GetterSetterCriterion.isASetter;
+import static fr.cnrs.i3s.sparks.composition.android.conflicts.MethodFilter.getAllMethods;
+import static fr.cnrs.i3s.sparks.composition.android.conflicts.MethodFilter.keepSetters;
 
 
 public class AddNPGuard extends AbstractProcessor<CtClass> {
-
-    IGSInliner igsInliner;
+    private List<CtMethod> settersToModify = new ArrayList<>();
 
     @Override
     public boolean isToBeProcessed(CtClass candidate) {
-        igsInliner = new IGSInliner();
-        return igsInliner.isToBeProcessed(candidate);
+        System.out.println("Starting analyse AddGuard...");
+
+        List<CtMethod> allMethods = getAllMethods(candidate);
+        settersToModify = keepSetters(allMethods);
+        System.out.println("Over analyse AddGuard.");
+
+        return !settersToModify.isEmpty();
     }
 
     @Override
     public void process(CtClass ctClass) {
-        //  Retrieve all getter/setters
-        Set<CtExecutable> ctExecutables = igsInliner.getIgsToInvocationsMap().keySet();
-        for (CtExecutable executable : ctExecutables) {
+        System.out.println("Starting to process AddGuard ...");
 
-            if (isASetter(executable)) {
-                CtParameter parameter = (CtParameter) executable.getParameters().get(0);
+        //  Retrieve all getter/setters
+        List<CtMethod> setters = settersToModify;
+        for (CtExecutable currentSetterMethod : setters) {
+
+            if (isASetter(currentSetterMethod)) {
+                CtParameter parameter = (CtParameter) currentSetterMethod.getParameters().get(0);
 
                 //  Create a if that wrap the actual body of the setter
                 CtIf ctIf = getFactory().createIf();
-                ctIf.setThenStatement(executable.getBody().clone());
+                ctIf.setThenStatement(currentSetterMethod.getBody().clone());
 
                 //  Add NON NULL guard with parameter name
                 ctIf.setCondition(getFactory().createCodeSnippetExpression(parameter.getSimpleName() + " != null"));
 
                 //  Replace body of the initial method by the if (that wraps everything)
-                executable.setBody(ctIf);
+                currentSetterMethod.setBody(ctIf);
             }
         }
+        System.out.println("Over processing AddGuard.");
+
     }
 }
